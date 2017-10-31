6281563c5380ce5ad9b8c01e7c8c9fda3551365d
sameesiddiqui <sameezsiddiqui@gmail.com>
2017-10-31 16:46:13 -0700

    local notification code

--- App.js
+++ App.js
@@ -35,18 +35,13 @@ export default class App extends React.Component {
       Asset.loadAsync([
       ]),
       Font.loadAsync([
-        // This is the font that we are using for our tab bar
         Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
+        { 'terminal': require('./assets/fonts/Inconsolata-Regular.ttf') },
       ]),
     ]);
   };
 
   _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
     console.warn(error);
   };
 
--- screens/CodeScreen.js
+++ screens/CodeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import Colors from '../constants/Colors'
 
 export default class CodeScreen extends React.Component {
   static navigationOptions = {
@@ -7,18 +8,72 @@ export default class CodeScreen extends React.Component {
   };
 
   render() {
+
     return (
       <ScrollView style={styles.container}>
-        <Text>Coming soon</Text>
+        <View style={{margin: 15}}>
+          {this._localInstructions()}
+        </View>
       </ScrollView>
     );
+  }
+
+  _localInstructions = () => {
+    const notification = {
+      title: 'this is your title!',
+      body: 'and your body',
+      data: { iWantData: 'yesPlease' },
+      ios: {
+        sound: true
+      },
+      android: {
+        sound: true
+      }
+    }
+
+    return (
+      <View>
+        <Text>
+          Creating a notification object is easy:
+        </Text>
+
+        <View style={styles.codeBlock}>
+          <Text style={styles.code}>
+            {'const notification = ' + JSON.stringify(notification, null, '\t')}
+          </Text>
+        </View>
+
+        <Text>
+          Then just schedule and send!
+        </Text>
+
+        <View style={styles.codeBlock}>
+          <Text style={styles.code}>
+            {'const scheduleOptions = {' + '\n\t' + '"time": Date.now() + 5000\n}'}
+            {`\n\n`}
+            {'Notifications.scheduleLocalNotificationAsync(' + '\n\t' + 'notification' +
+            '\n\t' + 'scheduleOptions\n)'}
+          </Text>
+        </View>
+      </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    // paddingTop: 15,
+    backgroundColor: Colors.offWhite,
   },
+  codeBlock: {
+    backgroundColor: '#eaeaea',
+    flex: 1,
+    marginTop: 10,
+    marginBottom: 10
+  },
+  code: {
+    fontFamily: 'terminal',
+    padding: 10
+  }
 });
